8466dbeea898905c8e09d24d7564a1e81b15eaf1
Azeezfasasi <fasasiazeez27@gmail.com>
2026-05-07 15:01:55 +0100

    Removed favicon.ico and portalfav.png, updated icon in layout.js to ardorfav.png

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
   title: 'Ardor Aegis - Security and Protection Solutions',
   description: 'Ardor Aegis provides cutting-edge security and protection solutions for communities. Trust us to keep your community safe and secure.',
   icons: {
-    icon: '/portalfav.png',
+    icon: '/ardorfav.png',
     apple: '/apple-icon.png',
   },
 }
